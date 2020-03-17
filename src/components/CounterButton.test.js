c9d0272e53cca296c58ed598145e5c86f0e6564b
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-17 08:32:34 +0100

    expanded test for incrementation

--- src/components/CounterButton.test.js
+++ src/components/CounterButton.test.js
@@ -13,4 +13,9 @@ it('correctly increments the counter', () => {
 
   wrapper.find('[id="counter"]').simulate('click')
   expect(wrapper.state()).toEqual({ count: 1 })
+  wrapper.find('[id="counter"]').simulate('click')
+  wrapper.find('[id="counter"]').simulate('click')
+  expect(wrapper.state()).toEqual({ count: 3 })
+  wrapper.find('[id="counter"]').simulate('keypress')
+  expect(wrapper.state()).toEqual({ count: 3 })
 })
