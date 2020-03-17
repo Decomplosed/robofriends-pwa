1464b3f643613b994e04106d056123556ddf530a
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-17 09:39:25 +0100

    re-organised the MainPage file structure

---
+++ src/components/MainPage/MainPage.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+
+import CardList from '../CardList'
+import SearchBox from '../SearchBox'
+import Scroll from '../Scroll'
+import ErrorBoundry from '../ErrorBoundry'
+import Header from '../Header'
+
+import './MainPage.css'
+
+class MainPage extends Component {
+  constructor() {
+    super()
+    this.state = {
+      count: 1
+    }
+  }
+  componentDidMount() {
+    this.props.onRequestRobots()
+  }
+
+  render() {
+    const { robots, searchField, onSearchChange, isPending } = this.props
+    const filteredRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchField.toLowerCase())
+    })
+    return (
+      <div className='tc'>
+        <Header count={this.state.count} />
+        <SearchBox searchChange={onSearchChange} />
+        <Scroll>
+          {isPending ? (
+            <h1>Loading</h1>
+          ) : (
+            <ErrorBoundry>
+              <CardList robots={filteredRobots} />
+            </ErrorBoundry>
+          )}
+        </Scroll>
+      </div>
+    )
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
