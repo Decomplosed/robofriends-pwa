491d6da1914bd3e8a26fc9ba62fc7b82b5d4fcc8
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-17 08:22:46 +0100

    wrote a test for CounterButton to check color

--- src/components/CounterButton.test.js
+++ src/components/CounterButton.test.js
@@ -1,3 +1,8 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import CounterButton from './CounterButton'
+
+it('expect to render CounterButton component', () => {
+  const mockColor = 'red'
+  expect(shallow(<CounterButton color={mockColor} />)).toMatchSnapshot()
+})
