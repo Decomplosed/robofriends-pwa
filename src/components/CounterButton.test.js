7fe59d8f398d68d3b0da4e675e337972a77e4dc3
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-17 08:29:17 +0100

    wrote a test for counter incrementation

--- src/components/CounterButton.test.js
+++ src/components/CounterButton.test.js
@@ -6,3 +6,11 @@ it('expect to render CounterButton component', () => {
   const mockColor = 'red'
   expect(shallow(<CounterButton color={mockColor} />)).toMatchSnapshot()
 })
+
+it('correctly increments the counter', () => {
+  const mockColor = 'red'
+  const wrapper = shallow(<CounterButton color={mockColor} />)
+
+  wrapper.find('[id="counter"]').simulate('click')
+  expect(wrapper.state()).toEqual({ count: 1 })
+})
