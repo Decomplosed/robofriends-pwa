2670c20a8914a75c02604dbc1dc19e9548a699b7
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-18 08:32:55 +0100

    added beforeEach

--- src/components/MainPage/MainPage.test.js
+++ src/components/MainPage/MainPage.test.js
@@ -2,6 +2,17 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import MainPage from './MainPage'
 
+let wrapper
+
+beforeEach(() => {
+  const mockProps = {
+    onRequestRobots: jest.fn(),
+    robots: [],
+    searchField: '',
+    isPending: false
+  }
+})
+
 it('expect to render App component', () => {
   const mockStore = {
     robots: [],
