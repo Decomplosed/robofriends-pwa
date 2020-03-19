906b422b7228dcdf74f7413aa16aed5b71eb431d
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-19 07:54:26 +0100

    imported all constants

--- src/actions.test.js
+++ src/actions.test.js
@@ -1,5 +1,14 @@
 import * as actions from './actions'
+import {
+  CHANGE_SEARCHFIELD,
+  REQUEST_ROBOTS_PENDING,
+  REQUEST_ROBOTS_SUCCESS,
+  REQUEST_ROBOTS_FAILED
+ } from './constants'
 
 it('SHould create an action to search robots', () => {
-  
+  const text = 'wooo'
+  const expectedAction = {
+    type:
+  }
 })
